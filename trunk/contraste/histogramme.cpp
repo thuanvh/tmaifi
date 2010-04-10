d0813195afd87aa9b842be84a8b3b12374af2631
thuanvh <thuanvh@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2010-04-10 08:47:05 +0000

    update commentaire

--- trunk/contraste/histogramme.cpp
+++ trunk/contraste/histogramme.cpp
@@ -95,13 +95,19 @@ CvPoint getCoordinate(int x, int y, int deltaX, int deltaY, int height) {
 const char* getFilePathName(const char* filepath, const char* newExtension) {
     std::string a(filepath);
     std::string b(newExtension);
+    // on n'obtient que quinze caracteres de le nom de fichier originaire
     return (a.substr(0,15) + b).c_str();
 }
+/*
+ * generer la chemin de fichier
+ *
+ */
 const char* getFilePathName(char** filepath,int length) {
     std::string a("");
     if(length>0)
     {
         a+=filepath[0];
+        // on n'obtient que quinze caracteres de le nom de fichier originaire
         a=a.substr(0,15);
     }
     for(int i=1; i<length; i++){
