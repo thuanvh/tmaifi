a2033f056e58b55c20ab9ffc288be28e0cab6c43
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2010-10-23 08:29:24 +0000

    Face regcog

---
+++ faceregc/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=faceregc
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/faceregc
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=faceregc.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/faceregc.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=faceregc
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/faceregc
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=faceregc.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/faceregc.tar
