a1ef7ff6f48b378d7c3e73d3d8f1dfca2a25273c
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2010-11-13 09:36:03 +0000

    image cherche

---
+++ imagechercher/skindetector.h
@@ -0,0 +1,54 @@
+/* 
+ * File:   skindetector.h
+ * Author: thuanvh
+ *
+ * Created on September 30, 2010, 9:12 PM
+ */
+
+#ifndef SKINDETECTOR_H
+#define	SKINDETECTOR_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include "skindetector.h"
+#include <stdlib.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stdio.h>
+
+#define FUNC_LEARNING 1
+#define FUNC_TEST 2
+
+#define STR_LEARNING "learning"
+#define STR_TEST "test"
+
+#define ERR_DIR_MISSING "Directory parametre is missing."
+#define ERR_DIR_OPEN "Could not open directory"
+#define ERR_FILE_MISSING "File parametre is missing."
+#define ERR_FILE_OPEN "Could not open file"
+#define ERR_FUNC_MISSING "Function parametre is missing."
+
+
+using namespace std;
+using namespace cv;
+
+void Learning(const char* dirPath, const char* histoName);
+void Testing(const char* testPath, const char* histoName, const char* fileOut, float thres, const char* refRefFilePath);
+void normalizeHistogram(MatND& hist);
+void displayHistogram(const MatND& hist, const char* name);
+void SaveHistograme(const MatND& hist, string name);
+void LoadHistograme(MatND& hist, string name);
+void SaveHistograme(const MatND& hist, const char* name);
+void LoadHistograme(MatND& hist, const char* name);
+int histNo(const MatND& hist);
+void gnuplot(const char *gnucommand);
+void SaveGnuPlotMatrix(const MatND& hist, const char* name);
+void img256To32(const Mat& src, Mat& dst);
+extern bool is_win_mode;
+extern int num_color;
+#endif	/* SKINDETECTOR_H */
+
