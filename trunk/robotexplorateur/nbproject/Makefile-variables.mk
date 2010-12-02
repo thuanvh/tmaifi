81b7b5380717914ffdbe7a7920b165d459f68ede
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2010-12-02 15:04:10 +0000

    chet tiet

---
+++ trunk/robotexplorateur/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=../test
+CND_ARTIFACT_NAME_Debug=robotexplorateur
+CND_ARTIFACT_PATH_Debug=../test/robotexplorateur
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=robotexplorateur.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/robotexplorateur.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=robotexplorateur
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/robotexplorateur
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=robotexplorateur.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/robotexplorateur.tar
