40a2b0dd1bdf9dc958a7a6351961fc9609949d86
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2010-12-02 15:04:10 +0000

    chet tiet

---
+++ robotexplorateur/exploration.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   exploration.h
+ * Author: thuanvh
+ *
+ * Created on November 25, 2010, 12:00 AM
+ */
+
+#ifndef EXPLORATION_H
+#define	EXPLORATION_H
+
+#define UNKNOWN 1
+#define OBSTACLE 0
+#define FREE 2
+
+#endif	/* EXPLORATION_H */
+
