a33af6aee9af670f232e1973260e35565c1f118c
thuanvh <thuanvh@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2010-12-29 06:36:25 +0000

    add limite of angle

--- trunk/jolirobot/jolirobot/main.cpp
+++ trunk/jolirobot/jolirobot/main.cpp
@@ -19,21 +19,21 @@ void changePerspective();
 float pz=0.0,px=0.0,Sin[360],Cos[360],theta=50;
 int xold,r=0;
 
-float angle_armxl0_corp=45;
-float angle_armxr0_corp=45;
+float angle_armxl0_corp=-195;
+float angle_armxr0_corp=-5;
 float angle_armyl0_corp=-45;
-float angle_armyr0_corp=-45;
+float angle_armyr0_corp=-215;
 float angle_armzl0_corp=45;
 float angle_armzr0_corp=45;
 
-float angle_armxl1_corp=45;
+float angle_armxl1_corp=-115;
 float angle_armxr1_corp=45;
-float angle_armyl1_corp=-45;
-float angle_armyr1_corp=-45;
+float angle_armyl1_corp=-85;
+float angle_armyr1_corp=-55;
 float angle_armzl1_corp=45;
 float angle_armzr1_corp=45;
 
-float angle_armxl2_corp=45;
+float angle_armxl2_corp=175;
 float angle_armxr2_corp=45;
 float angle_armyl2_corp=-45;
 float angle_armyr2_corp=-45;
@@ -123,6 +123,7 @@ void display()
   glRotated(-90,1,0,0);
   glColor3f(1.0,1.0,1.0);
 //  glRotated(-90,1,0,0);
+  //body
   gluCylinder(gluNewQuadric(),0.1,0.2,0.5,20,20);  
   glPushMatrix();
   /* neck */
@@ -140,90 +141,123 @@ void display()
   glutWireSphere(0.2,20,20);
   /* eyes */
   glPushMatrix();
-  glPopMatrix(); // eye 1
-  glPushMatrix();
-  glTranslated(0.1,0.2,0.1);
-  glColor3f(1.0,0,0);
-  //glutWireTorus(0.02,0.5,10,10);
-  glutWireSphere(0.05,20,20);
-  glPopMatrix(); // eye 2
-  glPushMatrix();
-  glTranslated(0.1,-0.2,0.1);
-  glColor3f(1.0,0,0);
-  //glutWireTorus(0.02,0.5,10,10);
-  glutWireSphere(0.05,20,20);
-  glPopMatrix(); //end eye
+  {
+    glPopMatrix(); // eye 1
+    glPushMatrix();
+    glTranslated(0.1,0.2,0.1);
+    glColor3f(1.0,0,0);
+    //glutWireTorus(0.02,0.5,10,10);
+    glutWireSphere(0.05,20,20);
+    glPopMatrix(); // eye 2
+    glPushMatrix();
+    glTranslated(0.1,-0.2,0.1);
+    glColor3f(1.0,0,0);
+    //glutWireTorus(0.02,0.5,10,10);
+    glutWireSphere(0.05,20,20);
+  }
+  glPopMatrix(); //end eye and head
   
   /* arm 1eft*/
   glPopMatrix();
-  glPushMatrix();
+  glPushMatrix();  
   glTranslated(0,0.2,0.5);
-  glRotatef(angle_armyl0_corp,0,-1,0);//angle of arm with the body
-  glRotatef(angle_armzl0_corp,0,0,-1);//angle of arm with the body
   glRotatef(angle_armxl0_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_armyl0_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_armzl0_corp,0,0,-1);//angle of arm with the body    
   glColor3f(1.0,0.5,1.0);
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
   glColor3f(0.5,0.5,0.2);
   glutWireSphere(0.05,20,20);
-  glRotatef(-45,0,-1,0);//angle of arm with the body
+  //glRotatef(-45,0,-1,0);//angle of arm with the body
   glColor3f(0.5,0.5,1);
+  glRotatef(angle_armxl1_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_armyl1_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_armzl1_corp,0,0,-1);//angle of arm with the body  
+  
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
-  glRotatef(-45,0,-1,0);
+  //glRotatef(-45,0,-1,0);
+  glRotatef(angle_armxl2_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_armyl2_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_armzl2_corp,0,0,-1);//angle of arm with the body  
   glColor3f(0.5,1,0.5);
   glutWireCone(0.1,0.1,20,1);//hand
   /* arm right*/
   glPopMatrix();
   glPushMatrix();
   glTranslated(0,-0.2,0.5);
-  glRotatef(angle_armyr0_corp,0,-1,0);//angle of arm with the body
-  glRotatef(angle_armzr0_corp,0,0,-1);//angle of arm with the body
   glRotatef(angle_armxr0_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_armyr0_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_armzr0_corp,0,0,-1);//angle of arm with the body  
   glColor3f(1.0,0.5,1.0);
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
   glColor3f(0.5,0.5,0.2);
   glutWireSphere(0.05,20,20);
-  glRotatef(-45,0,-1,0);//angle of arm with the body
+  //glRotatef(-45,0,-1,0);//angle of arm with the body
+  glRotatef(angle_armxr1_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_armyr1_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_armzr1_corp,0,0,-1);//angle of arm with the body  
   glColor3f(0.5,0.5,1);
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
-  glRotatef(-45,0,-1,0);
+//  glRotatef(-45,0,-1,0);
+  glRotatef(angle_armxr2_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_armyr2_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_armzr2_corp,0,0,-1);//angle of arm with the body  
   glColor3f(0.5,1,0.5);
   glutWireCone(0.1,0.1,20,1);//hand
-  /* leg 1*/
+  /* leg left*/
   glPopMatrix();
   glPushMatrix();
   glTranslated(0,0.2,0);
-  glRotatef(135,0,1,0);//angle of arm with the body
+  //glRotatef(135,0,1,0);//angle of arm with the body
+  glRotatef(angle_legxl0_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_legyl0_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legzl0_corp,0,0,-1);//angle of arm with the body  
   glColor3f(1.0,0.5,1.0);
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
   glColor3f(0.5,0.5,0.2);
   glutWireSphere(0.05,20,20);
-  glRotatef(-45,0,-1,0);//angle of arm with the body
+  //glRotatef(-45,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legxl1_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_legyl1_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legzl1_corp,0,0,-1);//angle of arm with the body  
   glColor3f(0.5,0.5,1);
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
-  glRotatef(-45,0,-1,0);
+  //glRotatef(-45,0,-1,0);
+  glRotatef(angle_legxl2_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_legyl2_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legzl2_corp,0,0,-1);//angle of arm with the body  
   glColor3f(0.5,1,0.5);
   glutWireCone(0.1,0.1,20,1);//foot  
   /* leg 2*/
   glPopMatrix();
   glPushMatrix();
   glTranslated(0,-0.2,0);
-  glRotatef(135,0,-1,0);//angle of arm with the body
+  //glRotatef(135,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legxr0_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_legyr0_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legzr0_corp,0,0,-1);//angle of arm with the body  
   glColor3f(1.0,0.5,1.0);
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
   glColor3f(0.5,0.5,0.2);
   glutWireSphere(0.05,20,20);
-  glRotatef(-45,0,-1,0);//angle of arm with the body
+  //glRotatef(-45,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legxr1_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_legyr1_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legzr1_corp,0,0,-1);//angle of arm with the body  
   glColor3f(0.5,0.5,1);
   gluCylinder(gluNewQuadric(),0.05,0.05,0.3,20,20);
   glTranslated(0,0,0.3);
-  glRotatef(-45,0,-1,0);
+  //glRotatef(-90,1,0,0);
+  glRotatef(angle_legxr2_corp,-1,0,0);//angle of arm with the body
+  glRotatef(angle_legyr2_corp,0,-1,0);//angle of arm with the body
+  glRotatef(angle_legzr2_corp,0,0,-1);//angle of arm with the body  
   glColor3f(0.5,1,0.5);
   glutWireCone(0.1,0.1,20,1);//foot  
   /* end robot */
@@ -303,18 +337,134 @@ void reset(){
   isRight=false;
   isHead=false;
 }
+void printOutConfigure(){
+  cout<<" angle_armxl0_corp:"<< angle_armxl0_corp<<endl;
+  cout<<" angle_armxr0_corp:"<< angle_armxr0_corp<<endl;
+  cout<<" angle_armyl0_corp:"<< angle_armyl0_corp<<endl;
+  cout<<" angle_armyr0_corp:"<< angle_armyr0_corp<<endl;
+  cout<<" angle_armzl0_corp:"<< angle_armzl0_corp<<endl;
+  cout<<" angle_armzr0_corp:"<< angle_armzr0_corp<<endl;
+  
+  cout<<" angle_armxl1_corp:"<< angle_armxl1_corp<<endl;
+  cout<<" angle_armxr1_corp:"<< angle_armxr1_corp<<endl;
+  cout<<" angle_armyl1_corp:"<< angle_armyl1_corp<<endl;
+  cout<<" angle_armyr1_corp:"<< angle_armyr1_corp<<endl;
+  cout<<" angle_armzl1_corp:"<< angle_armzl1_corp<<endl;
+  cout<<" angle_armzr1_corp:"<< angle_armzr1_corp<<endl;
+  
+  cout<<" angle_armxl2_corp:"<< angle_armxl2_corp<<endl;
+  cout<<" angle_armxr2_corp:"<< angle_armxr2_corp<<endl;
+  cout<<" angle_armyl2_corp:"<< angle_armyl2_corp<<endl;
+  cout<<" angle_armyr2_corp:"<< angle_armyr2_corp<<endl;
+  cout<<" angle_armzl2_corp:"<< angle_armzl2_corp<<endl;
+  cout<<" angle_armzr2_corp:"<< angle_armzr2_corp<<endl;
+  
+  cout<<" angle_legxl0_corp:"<< angle_legxl0_corp<<endl;
+  cout<<" angle_legxr0_corp:"<< angle_legxr0_corp<<endl;
+  cout<<" angle_legyl0_corp:"<< angle_legyl0_corp<<endl;
+  cout<<" angle_legyr0_corp:"<< angle_legyr0_corp<<endl;
+  cout<<" angle_legzl0_corp:"<< angle_legzl0_corp<<endl;
+  cout<<" angle_legzr0_corp:"<< angle_legzr0_corp<<endl;
+  
+  cout<<" angle_legxl1_corp:"<< angle_legxl1_corp<<endl;
+  cout<<" angle_legxr1_corp:"<< angle_legxr1_corp<<endl;
+  cout<<" angle_legyl1_corp:"<< angle_legyl1_corp<<endl;
+  cout<<" angle_legyr1_corp:"<< angle_legyr1_corp<<endl;
+  cout<<" angle_legzl1_corp:"<< angle_legzl1_corp<<endl;
+  cout<<" angle_legzr1_corp:"<< angle_legzr1_corp<<endl;
+  
+  cout<<" angle_legxl2_corp:"<< angle_legxl2_corp<<endl;
+  cout<<" angle_legxr2_corp:"<< angle_legxr2_corp<<endl;
+  cout<<" angle_legyl2_corp:"<< angle_legyl2_corp<<endl;
+  cout<<" angle_legyr2_corp:"<< angle_legyr2_corp<<endl;
+  cout<<" angle_legzl2_corp:"<< angle_legzl2_corp<<endl;
+  cout<<" angle_legzr2_corp:"<< angle_legzr2_corp<<endl;
+  
+  cout<<" angle_headx_corp:"<< angle_headx_corp<<endl;
+  cout<<" angle_heady_corp:"<< angle_heady_corp<<endl;
+  cout<<" angle_headz_corp:"<< angle_headz_corp<<endl;
+}
+void normaliseAngle(float& angle,int min,int max){
+  angle=(int)angle%360;
+  if(angle<0) angle=360+angle;
+  if(angle<min)
+    angle=min;
+  if(angle>max)
+    angle=max;
+}
+void normaliseAllAngle(){
+  normaliseAngle(angle_armxl0_corp,5,355);
+  normaliseAngle(angle_armxr0_corp,5,355);
+  normaliseAngle(angle_armyl0_corp,5,100);
+  normaliseAngle(angle_armyr0_corp,5,100);
+  normaliseAngle(angle_armzl0_corp,275,55);
+  normaliseAngle(angle_armzr0_corp,275,55);
+  
+  normaliseAngle(angle_armxl1_corp,0,150);
+  normaliseAngle(angle_armxr1_corp,0,150);
+  normaliseAngle(angle_armyl1_corp,0,0);
+  normaliseAngle(angle_armyr1_corp,0,0);
+  normaliseAngle(angle_armzl1_corp,0,180);
+  normaliseAngle(angle_armzr1_corp,0,180);
+  
+  normaliseAngle(angle_armxl2_corp,0,90);
+  normaliseAngle(angle_armxr2_corp,0,90);
+  normaliseAngle(angle_armyl2_corp,0,90);
+  normaliseAngle(angle_armyr2_corp,0,90);
+  normaliseAngle(angle_armzl2_corp,0,0);
+  normaliseAngle(angle_armzr2_corp,0,0);
+  
+  normaliseAngle(angle_legxl0_corp,0,90);
+  normaliseAngle(angle_legxr0_corp,0,90);
+  normaliseAngle(angle_legyl0_corp,0,70);
+  normaliseAngle(angle_legyr0_corp,0,70);
+  normaliseAngle(angle_legzl0_corp,0,0);
+  normaliseAngle(angle_legzr0_corp,0,0);
+  
+  normaliseAngle(angle_legxl1_corp,0,90);
+  normaliseAngle(angle_legxr1_corp,0,90);
+  normaliseAngle(angle_legyl1_corp,0,0);
+  normaliseAngle(angle_legyr1_corp,0,0);
+  normaliseAngle(angle_legzl1_corp,0,0);
+  normaliseAngle(angle_legzr1_corp,0,0);
+  
+  normaliseAngle(angle_legxl2_corp,0,90);
+  normaliseAngle(angle_legxr2_corp,0,90);
+  normaliseAngle(angle_legyl2_corp,0,70);
+  normaliseAngle(angle_legyr2_corp,0,70);
+  normaliseAngle(angle_legzl2_corp,0,0);
+  normaliseAngle(angle_legzr2_corp,0,0);
+  
+  normaliseAngle(angle_headx_corp,0,360);
+  normaliseAngle(angle_heady_corp,0,360);
+  normaliseAngle(angle_headz_corp,0,360);
+}
 void keyspecial(int key,int x,int y){
   switch(key){
     case GLUT_KEY_UP:
       isX=true;
       if(isArm){
 	if(isLeft){
-	  angle_armxl_corp+=10;
+	  if(elementId==0) angle_armxl0_corp+=10;
+	  else if(elementId==1) angle_armxl1_corp+=10;
+	  else if(elementId==2) angle_armxl2_corp+=10;
 	}else if(isRight){
-	  angle_armxr_corp+=10;
+	  if(elementId==0) angle_armxr0_corp+=10;
+	  else if(elementId==1) angle_armxr1_corp+=10;
+	  else if(elementId==2) angle_armxr2_corp+=10;
 	}
       }else if(isHead){
 	angle_headx_corp+=10;
+      }else if(isLeg){
+	if(isLeft){
+	  if(elementId==0) angle_legxl0_corp+=10;
+	  else if(elementId==1) angle_legxl1_corp+=10;
+	  else if(elementId==2) angle_legxl2_corp+=10;
+	}else if(isRight){
+	  if(elementId==0) angle_legxr0_corp+=10;
+	  else if(elementId==1) angle_legxr1_corp+=10;
+	  else if(elementId==2) angle_legxr2_corp+=10;
+	}
       }      
       glutPostRedisplay();
       break;
@@ -322,12 +472,26 @@ void keyspecial(int key,int x,int y){
       isX=true;
       if(isArm){
 	if(isLeft){
-	  angle_armxl_corp-=10;
+	  if(elementId==0) angle_armxl0_corp-=10;
+	  else if(elementId==1) angle_armxl1_corp-=10;
+	  else if(elementId==2) angle_armxl2_corp-=10;
 	}else if(isRight){
-	  angle_armxr_corp-=10;
+	  if(elementId==0) angle_armxr0_corp-=10;
+	  else if(elementId==1) angle_armxr1_corp-=10;
+	  else if(elementId==2) angle_armxr2_corp-=10;
 	}
       }else if(isHead){
 	angle_headx_corp-=10;
+      }else if(isLeg){
+	if(isLeft){
+	  if(elementId==0) angle_legxl0_corp-=10;
+	  else if(elementId==1) angle_legxl1_corp-=10;
+	  else if(elementId==2) angle_legxl2_corp-=10;
+	}else if(isRight){
+	  if(elementId==0) angle_legxr0_corp-=10;
+	  else if(elementId==1) angle_legxr1_corp-=10;
+	  else if(elementId==2) angle_legxr2_corp-=10;
+	}
       }
       glutPostRedisplay();
       break;
@@ -335,12 +499,26 @@ void keyspecial(int key,int x,int y){
       isY=true;
       if(isArm){
 	if(isLeft){
-	  angle_armyl_corp+=10;
+	  if(elementId==0) angle_armyl0_corp+=10;
+	  else if(elementId==1) angle_armyl1_corp+=10;
+	  else if(elementId==2) angle_armyl2_corp+=10;
 	}else if(isRight){
-	  angle_armyr_corp+=10;
+	  if(elementId==0) angle_armyr0_corp+=10;
+	  else if(elementId==1) angle_armyr1_corp+=10;
+	  else if(elementId==2) angle_armyr2_corp+=10;
 	}
       }else if(isHead){
 	angle_heady_corp+=10;
+      }else if(isLeg){
+	if(isLeft){
+	  if(elementId==0) angle_legyl0_corp+=10;
+	  else if(elementId==1) angle_legyl1_corp+=10;
+	  else if(elementId==2) angle_legyl2_corp+=10;
+	}else if(isRight){
+	  if(elementId==0) angle_legyr0_corp+=10;
+	  else if(elementId==1) angle_legyr1_corp+=10;
+	  else if(elementId==2) angle_legyr2_corp+=10;
+	}
       }
       glutPostRedisplay();
       break;
@@ -348,12 +526,26 @@ void keyspecial(int key,int x,int y){
       isY=true;
       if(isArm){
 	if(isLeft){
-	  angle_armyl_corp-=10;
+	  if(elementId==0) angle_armyl0_corp-=10;
+	  else if(elementId==1) angle_armyl1_corp-=10;
+	  else if(elementId==2) angle_armyl2_corp-=10;
 	}else if(isRight){
-	  angle_armyr_corp-=10;
+	  if(elementId==0) angle_armyr0_corp-=10;
+	  else if(elementId==1) angle_armyr1_corp-=10;
+	  else if(elementId==2) angle_armyr2_corp-=10;
 	}
       }else if(isHead){
 	angle_heady_corp-=10;
+      }else if(isLeg){
+	if(isLeft){
+	  if(elementId==0) angle_legyl0_corp-=10;
+	  else if(elementId==1) angle_legyl1_corp-=10;
+	  else if(elementId==2) angle_legyl2_corp-=10;
+	}else if(isRight){
+	  if(elementId==0) angle_legyr0_corp-=10;
+	  else if(elementId==1) angle_legyr1_corp-=10;
+	  else if(elementId==2) angle_legyr2_corp-=10;
+	}
       }
       glutPostRedisplay();
       break;
@@ -361,12 +553,26 @@ void keyspecial(int key,int x,int y){
       isZ=true;
       if(isArm){
 	if(isLeft){
-	  angle_armzl_corp+=10;
+	  if(elementId==0) angle_armzl0_corp+=10;
+	  else if(elementId==1) angle_armzl1_corp+=10;
+	  else if(elementId==2) angle_armzl2_corp+=10;
 	}else if(isRight){
-	  angle_armzr_corp+=10;
+	  if(elementId==0) angle_armzr0_corp+=10;
+	  else if(elementId==1) angle_armzr1_corp+=10;
+	  else if(elementId==2) angle_armzr2_corp+=10;
 	}
       }else if(isHead){
 	angle_headz_corp+=10;
+      }else if(isLeg){
+	if(isLeft){
+	  if(elementId==0) angle_legzl0_corp+=10;
+	  else if(elementId==1) angle_legzl1_corp+=10;
+	  else if(elementId==2) angle_legzl2_corp+=10;
+	}else if(isRight){
+	  if(elementId==0) angle_legzr0_corp+=10;
+	  else if(elementId==1) angle_legzr1_corp+=10;
+	  else if(elementId==2) angle_legzr2_corp+=10;
+	}
       }
       glutPostRedisplay();
       break;
@@ -374,20 +580,36 @@ void keyspecial(int key,int x,int y){
       isZ=true;
       if(isArm){
 	if(isLeft){
-	  angle_armzl_corp-=10;
+	  if(elementId==0) angle_armzl0_corp-=10;
+	  else if(elementId==1) angle_armzl1_corp-=10;
+	  else if(elementId==2) angle_armzl2_corp-=10;
 	}else if(isRight){
-	  angle_armzr_corp-=10;
+	  if(elementId==0) angle_armzr0_corp-=10;
+	  else if(elementId==1) angle_armzr1_corp-=10;
+	  else if(elementId==2) angle_armzr2_corp-=10;
 	}
       }else if(isHead){
 	angle_headz_corp-=10;
+      }else if(isLeg){
+	if(isLeft){
+	  if(elementId==0) angle_legzl0_corp-=10;
+	  else if(elementId==1) angle_legzl1_corp-=10;
+	  else if(elementId==2) angle_legzl2_corp-=10;
+	}else if(isRight){
+	  if(elementId==0) angle_legzr0_corp-=10;
+	  else if(elementId==1) angle_legzr1_corp-=10;
+	  else if(elementId==2) angle_legzr2_corp-=10;
+	}
       }
       glutPostRedisplay();
       break;
   }
+  normaliseAllAngle();
+  printOutConfigure();
 }
 void keyboard(unsigned char key,int x, int y)
 {
-  cout<<key<<" = "<<(int)key<<endl;
+//   cout<<key<<" = "<<(int)key<<endl;
   switch (key)
   {
     case 'q':
@@ -407,6 +629,7 @@ void keyboard(unsigned char key,int x, int y)
       isRight=true;
       break;
     case 'h':
+      reset();
       isHead=true;
       break;
     case '0':
