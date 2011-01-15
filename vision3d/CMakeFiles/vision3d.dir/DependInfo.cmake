9cf8eafb55f60d75f9a7212b2fe733f16d0403cd
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2011-01-15 11:44:46 +0000

    add surf

---
+++ vision3d/CMakeFiles/vision3d.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thuanvh/s1/computer_vision/tp4/vision3d/main.cpp" "/home/thuanvh/s1/computer_vision/tp4/vision3d/CMakeFiles/vision3d.dir/main.cpp.o"
+  "/home/thuanvh/s1/computer_vision/tp4/vision3d/vision.cpp" "/home/thuanvh/s1/computer_vision/tp4/vision3d/CMakeFiles/vision3d.dir/vision.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ vision3d/vision.h
@@ -0,0 +1,36 @@
+#ifndef VISION_H
+#define VISION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+class SurfVision3d {
+private:
+    void findPairs(const vector<KeyPoint>& objectKeypoints, const vector<float>& objectDescriptors,
+            const vector<KeyPoint>& imageKeypoints, const vector<float>& imageDescriptors, vector<int>& ptpairs, int size);
+    int findMatchingOne(const KeyPoint& keypoint,const vector<float>& vec,
+            const vector<KeyPoint>& model_keypoints,
+            const vector<float>& model_descriptors, int start, int length);
+    double compareSURFDescriptors(const vector<float>& d1, const vector<float>& d2, double best, int start1, int start2, int length);
+    bool isNear(const KeyPoint& kp1,const  KeyPoint& kp2);
+    double drawPoint(const vector<KeyPoint>& objectKeypoints, Mat& image, Mat& imageDraw);
+    double drawMatching2Image(const vector<int>& pointIndex,
+            const vector<KeyPoint>& objectKeypoints1, const vector<KeyPoint>& objectKeypoints2,
+            Mat& image1, Mat& image2, Mat& imageDraw);
+    double drawMatchingDeltaImage(const vector<int>& pointIndex,
+            const vector<KeyPoint>& objectKeypoints1, const vector<KeyPoint>& objectKeypoints2,
+            Mat& image1, Mat& imageDraw);
+    
+public:
+    int range;
+    
+    void matching(char* image1, char* image2);
+};
+
+#endif
