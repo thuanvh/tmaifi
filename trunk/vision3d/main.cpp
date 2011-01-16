27d4344e4db26e312fcb16bf5efcb5eb520c9c02
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2011-01-16 10:29:38 +0000

    add epipole

--- trunk/vision3d/main.cpp
+++ trunk/vision3d/main.cpp
@@ -21,21 +21,14 @@ void printHelp();
  */
 int main(int argc, char** argv) {
   try {
-    int typeOfFunction = 0;
-    char* outDir = NULL;
-    char* inDir = NULL;
+    int point_count = 20;
+    bool isQuiet = false;
+    char* outimg = NULL;
     char* img1 = NULL;
     char* img2 = NULL;
 
-    int cmd;
-    int adapt_wsize;
-    float adapt_tp;
-    int px_seuil_delta;
-    float px_seuil_px;
-    double lk_seuil_h, lk_seuil_p;
-    double histo_seuil;
-    int ct_apertureSize, ct_canny_contourDistance, ct_canny_contourThreshold, ct_canny_threshold1, ct_canny_threshold2;
-
+    int range=20;
+    
     // Analyser des parametres entres
     for (int i = 0; i < argc; i++) {
 
@@ -45,7 +38,7 @@ int main(int argc, char** argv) {
         if (i < argc) {
           img1 = argv[i];
         } else {
-//          throw ERR_FILE_MISSING;
+          //          throw ERR_FILE_MISSING;
         }
         continue;
       }
@@ -56,168 +49,49 @@ int main(int argc, char** argv) {
         if (i < argc) {
           img2 = argv[i];
         } else {
-//          throw ERR_FILE_MISSING;
+          //          throw ERR_FILE_MISSING;
         }
         continue;
       }
 
-//      // directory reference
-//      if (strcmp(argv[i], "-method") == 0) {
-//        i++;
-//        if (i < argc) {
-//          //timetolive = atof(argv[i]);
-//          if (strcmp(argv[i], STR_PIXEL) == 0) {
-//            method = PixelToPixel;
-//          } else if (strcmp(argv[i], STR_LIKELIHOOD) == 0) {
-//            method = LikelihoodRatio;
-//          } else if (strcmp(argv[i], STR_CONTOUR) == 0) {
-//            method = Contour;
-//          } else if (strcmp(argv[i], STR_HISTOGRAM) == 0) {
-//            method = Histogram;
-//          }
-//        } else {
-//          throw "method non specified";
-//        }
-//        continue;
-//      }
-//
-//      // directory reference
-//      if (strcmp(argv[i], "-px-seuil-delta") == 0) {
-//        i++;
-//        if (i < argc) {
-//          px_seuil_delta = atoi(argv[i]);
-//        } else {
+      // directory reference
+      if (strcmp(argv[i], "-out") == 0) {
+        // 	cout << "found" << endl;
+        i++;
+        if (i < argc) {
+          outimg = argv[i];
+        } else {
+          //          throw ERR_FILE_MISSING;
+        }
+        continue;
+      }
+
+      // directory reference
+      if (strcmp(argv[i], "-q") == 0) {
+        isQuiet = true;
+        continue;
+      }
+            // directory reference
+            if (strcmp(argv[i], "-point-count") == 0) {
+              i++;
+              if (i < argc) {
+                point_count = atoi(argv[i]);
+              } else {
+//                throw ERR_FILE_MISSING;
+              }
+              continue;
+            }
+            // directory reference
+      if (strcmp(argv[i], "-range") == 0) {
+        i++;
+        if (i < argc) {
+          range = atoi(argv[i]);
+        } else {
 //          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-px-seuil-px") == 0) {
-//        i++;
-//        if (i < argc) {
-//          px_seuil_px = atof(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-lk-seuil-h") == 0) {
-//        i++;
-//        if (i < argc) {
-//          lk_seuil_h = atof(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-lk-seuil-p") == 0) {
-//        i++;
-//        if (i < argc) {
-//          lk_seuil_p = atof(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-histo-seuil") == 0) {
-//        i++;
-//        if (i < argc) {
-//          histo_seuil = atof(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-ct-contourDistance") == 0) {
-//        i++;
-//        if (i < argc) {
-//          ct_canny_contourDistance = atoi(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-ct-contourThreshold") == 0) {
-//        i++;
-//        if (i < argc) {
-//          ct_canny_contourThreshold = atoi(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-ct-canny_threshold1") == 0) {
-//        i++;
-//        if (i < argc) {
-//          ct_canny_threshold1 = atoi(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-ct-apertureSize") == 0) {
-//        i++;
-//        if (i < argc) {
-//          ct_apertureSize = atoi(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      // directory reference
-//      if (strcmp(argv[i], "-ct-canny_threshold2") == 0) {
-//        i++;
-//        if (i < argc) {
-//          ct_canny_threshold2 = atoi(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//
-//      if (strcmp(argv[i], "-adapt-tp") == 0) {
-//        i++;
-//        if (i < argc) {
-//          adapt_tp = atof(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//      if (strcmp(argv[i], "-adapt-wsize") == 0) {
-//        i++;
-//        if (i < argc) {
-//          adapt_wsize = atoi(argv[i]);
-//        } else {
-//          throw ERR_FILE_MISSING;
-//        }
-//        continue;
-//      }
-//
-//      // directory reference
-//      if (strcmp(argv[i], "-cmd") == 0) {
-//        i++;
-//        if (i < argc) {
-//          //timetolive = atof(argv[i]);
-//          if (strcmp(argv[i], STR_CMD_TEST_ONE) == 0) {
-//            cmd = CMD_TEST_ONE;
-//          } else if (strcmp(argv[i], STR_CMD_TEST_ALL) == 0) {
-//            cmd = CMD_TEST_ALL;
-//          } else if (strcmp(argv[i], STR_CMD_TEST_ADAPT) == 0) {
-//            cmd = CMD_TEST_ADAPT;
-//          }
-//        } else {
-//          throw "method non specified";
-//        }
-//        continue;
-//      }
+        }
+        continue;
+      }
+      
 
       // type d'algo
       if (strcmp(argv[i], "--help") == 0) {
@@ -228,67 +102,69 @@ int main(int argc, char** argv) {
     }
 
     SurfVision3d surfv3;
-    surfv3.range=20;
-    surfv3.matching(img1,img2);
+    surfv3.range = range;
+    surfv3.isQuiet = isQuiet;
+    surfv3.point_count=point_count;
+    surfv3.matching(img1, img2, outimg);
     // init algorithm
-//    detectMethod* algo;
-//    switch (method) {
-//      case PixelToPixel:
-//        algo = new pixelToPixelMethod();
-//        ((pixelToPixelMethod*) algo)->seuil_delta = px_seuil_delta;
-//        ((pixelToPixelMethod*) algo)->seuil_pixel = px_seuil_px;
-//        break;
-//      case LikelihoodRatio:
-//        algo = new likelihoodRatioMethod();
-//        ((likelihoodRatioMethod*) algo)->seuil_h = lk_seuil_h;
-//        ((likelihoodRatioMethod*) algo)->seuil_p = lk_seuil_p;
-//        break;
-//      case Contour:
-//        algo = new contourMethod();
-//        ((contourMethod*) algo)->apertureSize = ct_apertureSize;
-//        ((contourMethod*) algo)->canny_threshold1 = ct_canny_threshold1;
-//        ((contourMethod*) algo)->canny_threshold2 = ct_canny_threshold2;
-//        ((contourMethod*) algo)->contourDistance = ct_canny_contourDistance;
-//        ((contourMethod*) algo)->contourThreshold = ct_canny_contourThreshold;
-//        break;
-//      case Histogram:
-//        algo = new histogramMethod();
-//        ((histogramMethod*) algo)->seuil_histo = histo_seuil;
-//        break;
-//    }
-//    switch (cmd) {
-//      case CMD_TEST_ONE:
-//      {
-//        plandetecter pdetecter;
-//        pdetecter.method = (AlgoType) method;
-//        pdetecter.algo = algo;
-//
-//        pdetecter.matrix = new matrixConfusion();
-//        // 	cout<<"ok";
-//
-//        plandetectertest pdtone;
-//        // 	cout<<inDir<<endl;
-//        pdtone.attachPlanDetecter(inDir, pdetecter.matrix);
-//        // 	pdetecter.matrix->print();
-//        pdetecter.detectPlan(inDir);
-//      }
-//        break;
-//      case CMD_TEST_ALL:
-//      {
-//        plandetectertest pdt;
-//        pdt.testPlanDetecter(inDir, (AlgoType) method, algo);
-//      }
-//        break;
-//      case CMD_TEST_ADAPT:
-//      {
-//        plandetectertest pdtadapt;
-//
-//        pdtadapt.testThresholdAdaptifAlgo(inDir, (AlgoType) method, algo, adapt_wsize, adapt_tp);
-//      }
-//        break;
-//      default:
-//        break;
-//    }
+    //    detectMethod* algo;
+    //    switch (method) {
+    //      case PixelToPixel:
+    //        algo = new pixelToPixelMethod();
+    //        ((pixelToPixelMethod*) algo)->seuil_delta = px_seuil_delta;
+    //        ((pixelToPixelMethod*) algo)->seuil_pixel = px_seuil_px;
+    //        break;
+    //      case LikelihoodRatio:
+    //        algo = new likelihoodRatioMethod();
+    //        ((likelihoodRatioMethod*) algo)->seuil_h = lk_seuil_h;
+    //        ((likelihoodRatioMethod*) algo)->seuil_p = lk_seuil_p;
+    //        break;
+    //      case Contour:
+    //        algo = new contourMethod();
+    //        ((contourMethod*) algo)->apertureSize = ct_apertureSize;
+    //        ((contourMethod*) algo)->canny_threshold1 = ct_canny_threshold1;
+    //        ((contourMethod*) algo)->canny_threshold2 = ct_canny_threshold2;
+    //        ((contourMethod*) algo)->contourDistance = ct_canny_contourDistance;
+    //        ((contourMethod*) algo)->contourThreshold = ct_canny_contourThreshold;
+    //        break;
+    //      case Histogram:
+    //        algo = new histogramMethod();
+    //        ((histogramMethod*) algo)->seuil_histo = histo_seuil;
+    //        break;
+    //    }
+    //    switch (cmd) {
+    //      case CMD_TEST_ONE:
+    //      {
+    //        plandetecter pdetecter;
+    //        pdetecter.method = (AlgoType) method;
+    //        pdetecter.algo = algo;
+    //
+    //        pdetecter.matrix = new matrixConfusion();
+    //        // 	cout<<"ok";
+    //
+    //        plandetectertest pdtone;
+    //        // 	cout<<inDir<<endl;
+    //        pdtone.attachPlanDetecter(inDir, pdetecter.matrix);
+    //        // 	pdetecter.matrix->print();
+    //        pdetecter.detectPlan(inDir);
+    //      }
+    //        break;
+    //      case CMD_TEST_ALL:
+    //      {
+    //        plandetectertest pdt;
+    //        pdt.testPlanDetecter(inDir, (AlgoType) method, algo);
+    //      }
+    //        break;
+    //      case CMD_TEST_ADAPT:
+    //      {
+    //        plandetectertest pdtadapt;
+    //
+    //        pdtadapt.testThresholdAdaptifAlgo(inDir, (AlgoType) method, algo, adapt_wsize, adapt_tp);
+    //      }
+    //        break;
+    //      default:
+    //        break;
+    //    }
 
   } catch (const char* e) {
     // error
--- trunk/vision3d/vision.h
+++ trunk/vision3d/vision.h
@@ -14,11 +14,11 @@ class SurfVision3d {
 private:
     void findPairs(const vector<KeyPoint>& objectKeypoints, const vector<float>& objectDescriptors,
             const vector<KeyPoint>& imageKeypoints, const vector<float>& imageDescriptors, vector<int>& ptpairs, int size);
-    int findMatchingOne(const KeyPoint& keypoint,const vector<float>& vec,
+    int findMatchingOne(const KeyPoint& keypoint, const vector<float>& vec,
             const vector<KeyPoint>& model_keypoints,
-            const vector<float>& model_descriptors, int start, int length);
+            const vector<float>& model_descriptors, int start, int length,double& distance);
     double compareSURFDescriptors(const vector<float>& d1, const vector<float>& d2, double best, int start1, int start2, int length);
-    bool isNear(const KeyPoint& kp1,const  KeyPoint& kp2);
+    bool isNear(const KeyPoint& kp1, const KeyPoint& kp2);
     double drawPoint(const vector<KeyPoint>& objectKeypoints, Mat& image, Mat& imageDraw);
     double drawMatching2Image(const vector<int>& pointIndex,
             const vector<KeyPoint>& objectKeypoints1, const vector<KeyPoint>& objectKeypoints2,
@@ -26,11 +26,15 @@ private:
     double drawMatchingDeltaImage(const vector<int>& pointIndex,
             const vector<KeyPoint>& objectKeypoints1, const vector<KeyPoint>& objectKeypoints2,
             Mat& image1, Mat& imageDraw);
-    
+    void calculateFondamentalMatrix(const vector<int>& pointIndex,
+            const vector<KeyPoint>& objectKeypoints1, const vector<KeyPoint>& objectKeypoints2,
+            const Mat& image1, const Mat& image2);
+    void displayEpipoleLine(const Mat& fundMatr, const Mat& image, const Mat& points, int startImage, int numPoints, const Mat& pointsThisImage,const char* windowName);
 public:
     int range;
-    
-    void matching(char* image1, char* image2);
+    bool isQuiet;
+    int point_count;
+    void matching(char* image1, char* image2, char* outimage = NULL);
 };
 
 #endif
