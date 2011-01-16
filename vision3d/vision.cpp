de3945c91e9b4e0fe63326e18349478d894e06dd
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2011-01-16 10:29:38 +0000

    add epipole

--- vision3d/vision.cpp
+++ vision3d/vision.cpp
@@ -19,7 +19,7 @@
 using namespace std;
 using namespace cv;
 
-void SurfVision3d::matching(char* strimage1, char* strimage2) {
+void SurfVision3d::matching(char* strimage1, char* strimage2, char* outimage) {
   Mat image1 = imread(strimage1, 1);
   Mat image2 = imread(strimage2, 1);
 
@@ -37,37 +37,78 @@ void SurfVision3d::matching(char* strimage1, char* strimage2) {
   cvtColor(image2, imagegray2, CV_RGB2GRAY);
   surf(imagegray1, Mat(), keypoints1, descriptors1);
   surf(imagegray2, Mat(), keypoints2, descriptors2);
-  Mat imagedraw1;
-  Mat imagedraw2;
-  drawPoint(keypoints1, image1, imagedraw1);
-  drawPoint(keypoints2, image2, imagedraw2);
-  imshow("image1", imagedraw1);
-  imshow("image2", imagedraw2);
+  if (!isQuiet) {
+    Mat imagedraw1;
+    Mat imagedraw2;
+    drawPoint(keypoints1, image1, imagedraw1);
+    drawPoint(keypoints2, image2, imagedraw2);
+    imshow("image1", imagedraw1);
+    imshow("image2", imagedraw2);
+  }
   int size = surf.descriptorSize();
   vector<int> ptpairs;
   //  cout<<"size:"<<size<<endl;
   findPairs(keypoints1, descriptors1, keypoints2, descriptors2, ptpairs, size);
-  Mat imagedraw2i;
+  if (!isQuiet) {
+    Mat imagedraw2i;
+    drawMatching2Image(ptpairs, keypoints1, keypoints2, image1, image2, imagedraw2i);
+    imshow("image correspondance 2", imagedraw2i);
+  }
   Mat imagedraw1i;
-  drawMatching2Image(ptpairs, keypoints1, keypoints2, image1, image2, imagedraw2i);
   drawMatchingDeltaImage(ptpairs, keypoints1, keypoints2, image1, imagedraw1i);
-  imshow("image correspondance 2", imagedraw2i);
-  imshow("image delta", imagedraw1i);
-  waitKey();
+  if (!isQuiet) {
+    imshow("image delta", imagedraw1i);
+
+  }
+  if (outimage != NULL) {
+    imwrite(outimage, imagedraw1i);
+  }
+  calculateFondamentalMatrix(ptpairs, keypoints1, keypoints2, image1, image2);
+  if (!isQuiet) {
+    waitKey();
+  }
+}
+
+void insertIntoMap(vector<double>& distanceArray, vector<int>& ptpairs, double distance, int index1, int index2) {
+  //  distanceArray.push_back(distance);
+  //  ptpairs.push_back(index1);
+  //  ptpairs.push_back(index2);
+  //  return;
+
+  vector<double>::iterator pos = lower_bound(distanceArray.begin(), distanceArray.end(), distance);
+
+  if (pos < distanceArray.end()) {    
+    int delta = pos - distanceArray.begin();
+//    cout << delta << endl;
+    distanceArray.insert(pos, distance);
+    ptpairs.insert(ptpairs.begin() + delta * 2, index1);
+    ptpairs.insert(ptpairs.begin() + delta * 2 + 1, index2);
+  } else {
+    distanceArray.push_back(distance);
+    ptpairs.push_back(index1);
+    ptpairs.push_back(index2);
+  }
+//  for (int i = 0; i < distanceArray.size(); i++) {
+//    cout << distanceArray[i] << " ";
+//  }
+//  cout << endl;
+//  getchar();
 }
 
 void SurfVision3d::findPairs(const vector<KeyPoint>& objectKeypoints, const vector<float>& objectDescriptors,
   const vector<KeyPoint>& imageKeypoints, const vector<float>& imageDescriptors, vector<int>& ptpairs, int size) {
   int i;
   ptpairs.clear();
+  vector<double> distVec;
   for (i = 0; i < objectKeypoints.size(); i++) {
     const KeyPoint* kp = (const KeyPoint*) &objectKeypoints[i];
-
+    double distance;
     int start1 = i*size;
-    int nearest_neighbor = findMatchingOne(*kp, objectDescriptors, imageKeypoints, imageDescriptors, start1, size);
+    int nearest_neighbor = findMatchingOne(*kp, objectDescriptors, imageKeypoints, imageDescriptors, start1, size, distance);
     if (nearest_neighbor != -1) {
-      ptpairs.push_back(i);
-      ptpairs.push_back(nearest_neighbor);
+      insertIntoMap(distVec, ptpairs, distance, i, nearest_neighbor);
+      //      ptpairs.push_back(i);
+      //      ptpairs.push_back(nearest_neighbor);
     }
   }
 }
@@ -83,7 +124,7 @@ bool SurfVision3d::isNear(const KeyPoint& kp1, const KeyPoint& kp2) {
 
 int SurfVision3d::findMatchingOne(const KeyPoint& keypoint, const vector<float>& vec,
   const vector<KeyPoint>& model_keypoints,
-  const vector<float>& model_descriptors, int start, int length) {
+  const vector<float>& model_descriptors, int start, int length, double& distance) {
 
   int i, neighbor = -1;
   double d, dist1 = 1e6, dist2 = 1e6;
@@ -107,8 +148,10 @@ int SurfVision3d::findMatchingOne(const KeyPoint& keypoint, const vector<float>&
       dist2 = d;
   }
 
-  if (dist1 < 0.5 * dist2)
+  if (dist1 < 0.5 * dist2) {
+    distance = dist1;
     return neighbor;
+  }
   return -1;
 }
 
@@ -142,10 +185,10 @@ double SurfVision3d::drawMatching2Image(const vector<int>& pointIndex,
   Mat& image1, Mat& image2, Mat& imageDraw) {
   int maxRow = image1.rows < image2.rows ? image2.rows : image1.rows;
   imageDraw = Mat::zeros(maxRow, image1.cols + image2.cols, CV_8UC3);
-//  Mat imageLeft = imageDraw.adjustROI(0, maxRow - image1.rows, 0, image2.cols);
-//  Mat imageRight = imageDraw.adjustROI(0, maxRow - image2.rows, image1.cols, 0);
-  Mat imageLeft=Mat(imageDraw,Rect(0,0,image1.cols,image1.rows));
-  Mat imageRight=Mat(imageDraw,Rect(image1.cols,0,image2.cols,image2.rows));
+  //  Mat imageLeft = imageDraw.adjustROI(0, maxRow - image1.rows, 0, image2.cols);
+  //  Mat imageRight = imageDraw.adjustROI(0, maxRow - image2.rows, image1.cols, 0);
+  Mat imageLeft = Mat(imageDraw, Rect(0, 0, image1.cols, image1.rows));
+  Mat imageRight = Mat(imageDraw, Rect(image1.cols, 0, image2.cols, image2.rows));
   image1.copyTo(imageLeft);
   image2.copyTo(imageRight);
 
@@ -171,301 +214,99 @@ double SurfVision3d::drawMatchingDeltaImage(const vector<int>& pointIndex,
     Point p1(objectKeypoints1[point1].pt.x, objectKeypoints1[point1].pt.y);
     Point p2(objectKeypoints2[point2].pt.x, objectKeypoints2[point2].pt.y);
     circle(imageDraw, p1, 2, Scalar(0, 0, 255), 1, 8, 0);
-//    circle(imageDraw, p2, 2, Scalar(255, 0, 0), 1, 8, 0);
+    //    circle(imageDraw, p2, 2, Scalar(255, 0, 0), 1, 8, 0);
     line(imageDraw, p1, p2, Scalar(0, 255, 255), 1, 8, 0);
   }
 }
-///*
-// * A Demo to OpenCV Implementation of SURF
-// * Further Information Refer to "SURF: Speed-Up Robust Feature"
-// * Author: Liu Liu
-// * liuliu.1987+opencv@gmail.com
-// */
-//
-//
-//
-//
-//// define whether to use approximate nearest-neighbor search
-//#define USE_FLANN
-//
-//
-//IplImage *image = 0;
-//
-//double compareSURFDescriptors(const float* d1, const float* d2, double best, int length) {
-//  double total_cost = 0;
-//  assert(length % 4 == 0);
-//  for (int i = 0; i < length; i += 4) {
-//    double t0 = d1[i] - d2[i];
-//    double t1 = d1[i + 1] - d2[i + 1];
-//    double t2 = d1[i + 2] - d2[i + 2];
-//    double t3 = d1[i + 3] - d2[i + 3];
-//    total_cost += t0 * t0 + t1 * t1 + t2 * t2 + t3*t3;
-//    if (total_cost > best)
-//      break;
-//  }
-//  return total_cost;
-//}
-//
-//int naiveNearestNeighbor(const float* vec, int laplacian,
-//  const CvSeq* model_keypoints,
-//  const CvSeq* model_descriptors) {
-//  int length = (int) (model_descriptors->elem_size / sizeof (float));
-//  int i, neighbor = -1;
-//  double d, dist1 = 1e6, dist2 = 1e6;
-//  CvSeqReader reader, kreader;
-//  cvStartReadSeq(model_keypoints, &kreader, 0);
-//  cvStartReadSeq(model_descriptors, &reader, 0);
-//
-//  for (i = 0; i < model_descriptors->total; i++) {
-//    const CvSURFPoint* kp = (const CvSURFPoint*) kreader.ptr;
-//    const float* mvec = (const float*) reader.ptr;
-//    CV_NEXT_SEQ_ELEM(kreader.seq->elem_size, kreader);
-//    CV_NEXT_SEQ_ELEM(reader.seq->elem_size, reader);
-//    if (laplacian != kp->laplacian)
-//      continue;
-//    d = compareSURFDescriptors(vec, mvec, dist2, length);
-//    if (d < dist1) {
-//      dist2 = dist1;
-//      dist1 = d;
-//      neighbor = i;
-//    } else if (d < dist2)
-//      dist2 = d;
-//  }
-//  if (dist1 < 0.6 * dist2)
-//    return neighbor;
-//  return -1;
-//}
-//
-//void findPairs(const CvSeq* objectKeypoints, const CvSeq* objectDescriptors,
-//  const CvSeq* imageKeypoints, const CvSeq* imageDescriptors, vector<int>& ptpairs) {
-//  int i;
-//  CvSeqReader reader, kreader;
-//  cvStartReadSeq(objectKeypoints, &kreader);
-//  cvStartReadSeq(objectDescriptors, &reader);
-//  ptpairs.clear();
-//
-//  for (i = 0; i < objectDescriptors->total; i++) {
-//    const CvSURFPoint* kp = (const CvSURFPoint*) kreader.ptr;
-//    const float* descriptor = (const float*) reader.ptr;
-//    CV_NEXT_SEQ_ELEM(kreader.seq->elem_size, kreader);
-//    CV_NEXT_SEQ_ELEM(reader.seq->elem_size, reader);
-//    int nearest_neighbor = naiveNearestNeighbor(descriptor, kp->laplacian, imageKeypoints, imageDescriptors);
-//    if (nearest_neighbor >= 0) {
-//      ptpairs.push_back(i);
-//      ptpairs.push_back(nearest_neighbor);
-//    }
-//  }
-//}
-//
-//void
-//flannFindPairs(const CvSeq*, const CvSeq* objectDescriptors,
-//  const CvSeq*, const CvSeq* imageDescriptors, vector<int>& ptpairs) {
-//  int length = (int) (objectDescriptors->elem_size / sizeof (float));
-//
-//  cv::Mat m_object(objectDescriptors->total, length, CV_32F);
-//  cv::Mat m_image(imageDescriptors->total, length, CV_32F);
-//
-//
-//  // copy descriptors
-//  CvSeqReader obj_reader;
-//  float* obj_ptr = m_object.ptr<float>(0);
-//  cvStartReadSeq(objectDescriptors, &obj_reader);
-//  for (int i = 0; i < objectDescriptors->total; i++) {
-//    const float* descriptor = (const float*) obj_reader.ptr;
-//    CV_NEXT_SEQ_ELEM(obj_reader.seq->elem_size, obj_reader);
-//    memcpy(obj_ptr, descriptor, length * sizeof (float));
-//    obj_ptr += length;
-//  }
-//  CvSeqReader img_reader;
-//  float* img_ptr = m_image.ptr<float>(0);
-//  cvStartReadSeq(imageDescriptors, &img_reader);
-//  for (int i = 0; i < imageDescriptors->total; i++) {
-//    const float* descriptor = (const float*) img_reader.ptr;
-//    CV_NEXT_SEQ_ELEM(img_reader.seq->elem_size, img_reader);
-//    memcpy(img_ptr, descriptor, length * sizeof (float));
-//    img_ptr += length;
-//  }
-//
-//  // find nearest neighbors using FLANN
-//  cv::Mat m_indices(objectDescriptors->total, 2, CV_32S);
-//  cv::Mat m_dists(objectDescriptors->total, 2, CV_32F);
-//  cv::flann::Index flann_index(m_image, cv::flann::KDTreeIndexParams(4)); // using 4 randomized kdtrees
-//  flann_index.knnSearch(m_object, m_indices, m_dists, 2, cv::flann::SearchParams(64)); // maximum number of leafs checked
-//
-//  int* indices_ptr = m_indices.ptr<int>(0);
-//  float* dists_ptr = m_dists.ptr<float>(0);
-//  for (int i = 0; i < m_indices.rows; ++i) {
-//    if (dists_ptr[2 * i] < 0.6 * dists_ptr[2 * i + 1]) {
-//      ptpairs.push_back(i);
-//      ptpairs.push_back(indices_ptr[2 * i]);
-//    }
-//  }
-//}
-//
-///* a rough implementation for object location */
-//int
-//locatePlanarObject(const CvSeq* objectKeypoints, const CvSeq* objectDescriptors,
-//  const CvSeq* imageKeypoints, const CvSeq* imageDescriptors,
-//  const CvPoint src_corners[4], CvPoint dst_corners[4]) {
-//  double h[9];
-//  CvMat _h = cvMat(3, 3, CV_64F, h);
-//  vector<int> ptpairs;
-//  vector<CvPoint2D32f> pt1, pt2;
-//  CvMat _pt1, _pt2;
-//  int i, n;
-//
-//#ifdef USE_FLANN
-//  flannFindPairs(objectKeypoints, objectDescriptors, imageKeypoints, imageDescriptors, ptpairs);
-//#else
-//  findPairs(objectKeypoints, objectDescriptors, imageKeypoints, imageDescriptors, ptpairs);
-//#endif
-//
-//  n = ptpairs.size() / 2;
-//  if (n < 4)
-//    return 0;
-//
-//  pt1.resize(n);
-//  pt2.resize(n);
-//  for (i = 0; i < n; i++) {
-//    pt1[i] = ((CvSURFPoint*) cvGetSeqElem(objectKeypoints, ptpairs[i * 2]))->pt;
-//    pt2[i] = ((CvSURFPoint*) cvGetSeqElem(imageKeypoints, ptpairs[i * 2 + 1]))->pt;
-//  }
-//
-//  _pt1 = cvMat(1, n, CV_32FC2, &pt1[0]);
-//  _pt2 = cvMat(1, n, CV_32FC2, &pt2[0]);
-//  if (!cvFindHomography(&_pt1, &_pt2, &_h, CV_RANSAC, 5))
-//    return 0;
-//
-//  for (i = 0; i < 4; i++) {
-//    double x = src_corners[i].x, y = src_corners[i].y;
-//    double Z = 1. / (h[6] * x + h[7] * y + h[8]);
-//    double X = (h[0] * x + h[1] * y + h[2]) * Z;
-//    double Y = (h[3] * x + h[4] * y + h[5]) * Z;
-//    dst_corners[i] = cvPoint(cvRound(X), cvRound(Y));
-//  }
-//
-//  return 1;
-//}
-//
-//int main(int argc, char** argv) {
-//  const char* object_filename = argc == 3 ? argv[1] : "box.png";
-//  const char* scene_filename = argc == 3 ? argv[2] : "box_in_scene.png";
-//
-//  CvMemStorage* storage = cvCreateMemStorage(0);
-//
-//  cvNamedWindow("Object", 1);
-//  cvNamedWindow("Object Correspond", 1);
-//
-//  static CvScalar colors[] = {
-//    {
-//      {0, 0, 255}
-//    },
-//    {
-//      {0, 128, 255}
-//    },
-//    {
-//      {0, 255, 255}
-//    },
-//    {
-//      {0, 255, 0}
-//    },
-//    {
-//      {255, 128, 0}
-//    },
-//    {
-//      {255, 255, 0}
-//    },
-//    {
-//      {255, 0, 0}
-//    },
-//    {
-//      {255, 0, 255}
-//    },
-//    {
-//      {255, 255, 255}
-//    }
-//  };
-//
-//  IplImage* object = cvLoadImage(object_filename, CV_LOAD_IMAGE_GRAYSCALE);
-//  IplImage* image = cvLoadImage(scene_filename, CV_LOAD_IMAGE_GRAYSCALE);
-//  if (!object || !image) {
-//    fprintf(stderr, "Can not load %s and/or %s\n"
-//      "Usage: find_obj [<object_filename> <scene_filename>]\n",
-//      object_filename, scene_filename);
-//    exit(-1);
-//  }
-//  IplImage* object_color = cvCreateImage(cvGetSize(object), 8, 3);
-//  cvCvtColor(object, object_color, CV_GRAY2BGR);
-//
-//  CvSeq *objectKeypoints = 0, *objectDescriptors = 0;
-//  CvSeq *imageKeypoints = 0, *imageDescriptors = 0;
-//  int i;
-//  CvSURFParams params = cvSURFParams(500, 1);
-//
-//  double tt = (double) cvGetTickCount();
-//  cvExtractSURF(object, 0, &objectKeypoints, &objectDescriptors, storage, params);
-//  printf("Object Descriptors: %d\n", objectDescriptors->total);
-//  cvExtractSURF(image, 0, &imageKeypoints, &imageDescriptors, storage, params);
-//  printf("Image Descriptors: %d\n", imageDescriptors->total);
-//  tt = (double) cvGetTickCount() - tt;
-//  printf("Extraction time = %gms\n", tt / (cvGetTickFrequency()*1000.));
-//  CvPoint src_corners[4] = {
-//    {0, 0},
-//    {object->width, 0},
-//    {object->width, object->height},
-//    {0, object->height}
-//  };
-//  CvPoint dst_corners[4];
-//  IplImage* correspond = cvCreateImage(cvSize(image->width, object->height + image->height), 8, 1);
-//  cvSetImageROI(correspond, cvRect(0, 0, object->width, object->height));
-//  cvCopy(object, correspond);
-//  cvSetImageROI(correspond, cvRect(0, object->height, correspond->width, correspond->height));
-//  cvCopy(image, correspond);
-//  cvResetImageROI(correspond);
-//
-//#ifdef USE_FLANN
-//  printf("Using approximate nearest neighbor search\n");
-//#endif
-//
-//  if (locatePlanarObject(objectKeypoints, objectDescriptors, imageKeypoints,
-//    imageDescriptors, src_corners, dst_corners)) {
-//    for (i = 0; i < 4; i++) {
-//      CvPoint r1 = dst_corners[i % 4];
-//      CvPoint r2 = dst_corners[(i + 1) % 4];
-//      cvLine(correspond, cvPoint(r1.x, r1.y + object->height),
-//        cvPoint(r2.x, r2.y + object->height), colors[8]);
-//    }
-//  }
-//  vector<int> ptpairs;
-//#ifdef USE_FLANN
-//  flannFindPairs(objectKeypoints, objectDescriptors, imageKeypoints, imageDescriptors, ptpairs);
-//#else
-//  findPairs(objectKeypoints, objectDescriptors, imageKeypoints, imageDescriptors, ptpairs);
-//#endif
-//  for (i = 0; i < (int) ptpairs.size(); i += 2) {
-//    CvSURFPoint* r1 = (CvSURFPoint*) cvGetSeqElem(objectKeypoints, ptpairs[i]);
-//    CvSURFPoint* r2 = (CvSURFPoint*) cvGetSeqElem(imageKeypoints, ptpairs[i + 1]);
-//    cvLine(correspond, cvPointFrom32f(r1->pt),
-//      cvPoint(cvRound(r2->pt.x), cvRound(r2->pt.y + object->height)), colors[8]);
-//  }
-//
-//  cvShowImage("Object Correspond", correspond);
-//  for (i = 0; i < objectKeypoints->total; i++) {
-//    CvSURFPoint* r = (CvSURFPoint*) cvGetSeqElem(objectKeypoints, i);
-//    CvPoint center;
-//    int radius;
-//    center.x = cvRound(r->pt.x);
-//    center.y = cvRound(r->pt.y);
-//    radius = cvRound(r->size * 1.2 / 9. * 2);
-//    cvCircle(object_color, center, radius, colors[0], 1, 8, 0);
-//  }
-//  cvShowImage("Object", object_color);
-//
-//  cvWaitKey(0);
-//
-//  cvDestroyWindow("Object");
-//  cvDestroyWindow("Object SURF");
-//  cvDestroyWindow("Object Correspond");
-//
-//  return 0;
-//}
+
+void SurfVision3d::calculateFondamentalMatrix(const vector<int>& pointIndex,
+  const vector<KeyPoint>& objectKeypoints1, const vector<KeyPoint>& objectKeypoints2,
+  const Mat& image1, const Mat& image2) {
+
+  //create the output fundamental matrix
+  int numPoints = point_count < pointIndex.size() ? point_count : pointIndex.size();
+  Mat fundMatr = cvCreateMat(3, 3, CV_32F);
+  Mat points1 = Mat::zeros(1, numPoints, CV_32FC2);
+  Mat points2 = Mat::zeros(1, numPoints, CV_32FC2);
+  //  Mat status = Mat::zeros(1,point_count,CV_8UC1);
+
+  for (int i = 0; i < numPoints * 2; i += 2) {
+    int p1 = pointIndex[i];
+    int p2 = pointIndex[i + 1];
+
+    ((float*) points1.data)[i] = objectKeypoints1[p1].pt.x;
+    ((float*) points1.data)[i + 1] = objectKeypoints1[p1].pt.y;
+
+    ((float*) points2.data)[i] = objectKeypoints2[p2].pt.x;
+    ((float*) points2.data)[i + 1] = objectKeypoints2[p2].pt.y;
+
+    //    cout << p1 << " " << objectKeypoints1[p1].pt.x << "," << objectKeypoints1[p1].pt.y << " ";
+    //    cout << p2 << " " << objectKeypoints2[p2].pt.x << "," << objectKeypoints2[p2].pt.y << endl;
+  }
+  fundMatr = findFundamentalMat(points1, points2, CV_FM_RANSAC, 1.0, 0.99);
+  cout << "Fundamental Matrix:" << endl;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      cout << fundMatr.at<float>(i, j) << "\t";
+    }
+    cout << endl;
+  }
+
+  displayEpipoleLine(fundMatr, image1, points2, 2, numPoints, points1, "image epipole 1");
+  displayEpipoleLine(fundMatr, image2, points1, 1, numPoints, points2, "image epipole 2");
+}
+
+void SurfVision3d::displayEpipoleLine(const Mat& fundMatr, const Mat& image, const Mat& points,
+  int startImage, int numPoints, const Mat& pointsThisImage, const char* windowName) {
+  //now visualize the fundamental matrix
+
+  vector<Vec3f> corrLines;
+
+  //specify which direction to compute epipolar lines
+  //  int startImage = 2;
+  computeCorrespondEpilines(points,
+    startImage, //means points are in image 1
+    fundMatr,
+    corrLines);
+
+  //create output window
+  //  char windowName[100];
+  //  strcpy_s(windowName, ”Output Window”);
+  namedWindow(windowName, CV_WINDOW_AUTOSIZE);
+
+  //for all the points set the point and corresponding epipolar line
+  //and determine where the epipolar line intersects the image plane
+  //then display all this info
+
+  int width = image.cols;
+  int height = image.rows;
+  Mat imgcpy = image.clone();
+  for (int i1 = 0; i1 < numPoints; i1++) {
+    float a = corrLines[i1][0];
+    float b = corrLines[i1][1];
+    float c = corrLines[i1][2];
+    //    cout << a << "," << b << "," << c << endl;
+
+    Point epipolarLinePoint[2]; //, epipolarLinePoint2;
+    Point points[4];
+    points[0] = b == 0 ? Point(0, -1) : Point(0, round(-c / b));
+    points[1] = a == 0 ? Point(-1, 0) : Point(round(-c / a), 0);
+    points[2] = b == 0 ? Point(width - 1, -1) : Point(width - 1, round((-c - a * width) / b));
+    points[3] = a == 0 ? Point(-1, height - 1) : Point(round((-c - b * height) / a), height - 1);
+
+    int j = 0;
+    for (int i = 0; i < 4; i++) {
+      if (j >= 2) break;
+      //      cout<<"Points:"<<points[i].x<<"*"<<points[i].y<<endl;
+      if (points[i].x < width && points[i].x >= 0 && points[i].y < height && points[i].y >= 0)
+        epipolarLinePoint[j++] = points[i];
+    }
+    //    cout<<"="<<j<<endl;
+    if (j < 2)
+      continue;
+    circle(imgcpy, Point(((float*) pointsThisImage.data)[i1 * 2], ((float*) pointsThisImage.data)[i1 * 2 + 1]), 2, Scalar(0, 0, 255), 1, 8, 0);
+    line(imgcpy, epipolarLinePoint[0], epipolarLinePoint[1], CV_RGB(0, 255, 0));
+
+  }
+  imshow(windowName, imgcpy);
+}
