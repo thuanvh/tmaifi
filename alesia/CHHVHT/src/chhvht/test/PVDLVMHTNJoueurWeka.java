ed7bb23ae7d36db47cf74d882eaf5c0f3fc777a2
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2011-02-11 04:08:08 +0000

    add opponent

---
+++ alesia/CHHVHT/src/chhvht/test/PVDLVMHTNJoueurWeka.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chhvht.test;
+
+import player.*;
+
+/**
+ *
+ * @author thuanvh
+ */
+public class PVDLVMHTNJoueurWeka implements chhvht.Joueur{
+
+    player.Player joueur;
+
+    public PVDLVMHTNJoueurWeka() {
+//        String myProgram = "player.PlayerRandomWekaTree";
+
+        // NOTRE PROGRAMME SE COMPOSE 5 TYPE DE JOUEUR. VOUS POUVEZ TESTER
+        // EN CHANGEANT LE NOM DE TYPE JOUEUR COMME LES SUIVANTES:
+        //String myProgram = "player.Player5";
+        //String myProgram = "player.PlayerAdaptif",;
+        //String myProgram = "player.PlayerSimplestTDMethod";
+        //String myProgram = "player.Ramdom",
+        joueur = (Player)new PlayerRandomWekaTree();
+        joueur.nextMatch(1000);
+    }
+
+    public void NewMatch(int nbParties) {
+    }
+
+    public int NextMove(int moneyA, int p, int moneyB) {
+        return joueur.nextMove(moneyA, p, moneyB);
+    }
+
+    public String getAuteur() {
+        return "PVDLVMHTN.Weka";
+    }
+
+    public void LoadPlayer(String file) {
+    }
+
+    public void SaveMe(String filename) {
+    }
+
+    public void endMatch(int r) {
+    }
+}
