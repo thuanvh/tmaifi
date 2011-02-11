caba81e20e5b3279bde5eb1f5436a916ffc9d350
thuanvh@gmail.com <thuanvh@gmail.com@c512ba71-6aa1-87ce-671d-f65cc9c643ba>
2011-02-11 02:38:33 +0000

    add opponent

---
+++ trunk/alesia/CHHVHT/src/chhvht/test/TXHVQHJoueur.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chhvht.test;
+
+import txhvqhv01.*;
+
+/**
+ *
+ * @author thuanvh
+ */
+public class TXHVQHJoueur implements chhvht.Joueur {
+
+    txhvqhv01.TXHVQHJoueur player;
+
+    public TXHVQHJoueur() {
+        player = new txhvqhv01.TXHVQHJoueur();
+        player.LoadPlayer("TXHVQHV01.txt");
+    }
+
+    public void NewMatch(int nbParties) {
+    }
+
+    public int NextMove(int moneyA, int p, int moneyB) {
+        Node nodeCurrent = new Node();
+        // 5- Votre prix exist, 6 - Mon prix exist, 1 - Votre status, 0 - Ma status, status = 3 --> gagne
+        nodeCurrent.setValueNode(moneyB, moneyA, p%3, p/3);
+        int choice = player.ChoiceBestPrix(nodeCurrent);
+        return choice;
+    }
+
+    public String getAuteur() {
+        return "TXHVQH";
+    }
+
+    public void endMatch(int r) {
+    }
+
+    public void LoadPlayer(String file) {
+    }
+
+    public void SaveMe(String filename) {
+    }
+}
